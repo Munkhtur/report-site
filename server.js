6f6490cc27370697e003d0ba4944e2a66e6f9b4b
Tur <gmunkhtur@gmail.com>
2020-04-11 18:43:56 +1000

    last

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -22,12 +23,20 @@ app.use(express.json({ extened: false }));
 //   next();
 // });
 
-app.get('/', (req, res) => res.send('API running'));
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/categories', require('./routes/api/categories'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  //serve the html file in build
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
